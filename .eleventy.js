8b39c7c7166b268be353cda8d02e07fcd59fb5d2
Hadleigh Jae Bills <hjb5471@psu.edu>
2024-11-12 21:13:22 -0500

    uh

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,10 @@ module.exports = function (eleventyConfig){
     eleventyConfig.addPassthroughCopy("_src/_includes/");
     eleventyConfig.addPassthroughCopy("./_src/css/");
 
-        return{
-            dir: {
-                input: "_src",
-                output: "_site"
-            },
-        };
+    return{
+        dir: {
+            input: "_src",
+            output: "_site"
+        },
     };
+};
